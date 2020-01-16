4bcc37c82fdc3b6b279e04d21a025eca784fa26d
mRahulJain <rj.jain.me@gmail.com>
2020-01-17 00:46:45 +0530

    making ui

---
+++ src/main/java/com/mvvmzomato/mvvmimplementation/view/InitialAct.kt
@@ -0,0 +1,13 @@
+package com.mvvmzomato.mvvmimplementation.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mvvmzomato.mvvmimplementation.R
+
+class InitialAct : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_initial)
+    }
+}
--- src/main/java/com/mvvmzomato/mvvmimplementation/view/MainActivity.kt
+++ src/main/java/com/mvvmzomato/mvvmimplementation/view/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.mvvmzomato.mvvmimplementation.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mvvmzomato.mvvmimplementation.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnGetStarted.setOnClickListener {
+            val intent = Intent(this, InitialAct::class.java)
+            startActivity(intent)
+        }
     }
 }
