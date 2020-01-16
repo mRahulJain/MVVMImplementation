2ae0290de439d5c5bf278f63d2f619cdd18d540f
mRahulJain <rj.jain.me@gmail.com>
2020-01-16 11:56:21 +0530

    welcome page

---
+++ src/main/java/com/mvvmzomato/mvvmimplementation/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.mvvmzomato.mvvmimplementation.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mvvmzomato.mvvmimplementation.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
